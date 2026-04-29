d5bee5e444f17d8ee6ebc24a825b68ab88f713a1
camalzadeelvin <camalzadeelvin@STHQ011A-01>
2026-04-29 10:17:14 +0400

    Add project files.

---
+++ HttpTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using HttpTest.Services;
+using System.Threading.Tasks;
+
+namespace HttpTest
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            UserService service = new UserService();
+            //var users = await service.GetUsers();
+            //foreach (var u in users)
+            //{
+            //    Console.WriteLine(u);
+            //}
+
+
+            var user = await service.GetUser(1);
+            Console.WriteLine(user);
+        }
+    }
+}
---
+++ HttpTest/Services/UserService.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpTest.Services
+{
+
+    public class Rootobject
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public User[] data { get; set; }
+        public Support support { get; set; }
+        public _Meta _meta { get; set; }
+    }
+
+    public class RootobjectSingle
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public User data { get; set; }
+        public Support support { get; set; }
+        public _Meta _meta { get; set; }
+    }
+
+    public class Support
+    {
+        public string url { get; set; }
+        public string text { get; set; }
+    }
+
+    public class _Meta
+    {
+        public string powered_by { get; set; }
+        public string docs_url { get; set; }
+        public string upgrade_url { get; set; }
+        public string example_url { get; set; }
+        public string variant { get; set; }
+        public string message { get; set; }
+        public Cta cta { get; set; }
+        public string context { get; set; }
+    }
+
+    public class Cta
+    {
+        public string label { get; set; }
+        public string url { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+
+        public override string ToString()
+        {
+            return $@"
+    ID : {id}
+    EMAIL : {email}
+    Firstname : {first_name}
+    Lastname : {last_name}
+    Avatar : {avatar}
+";
+        }
+    }
+
+
+    public class UserService
+    {
+        private readonly string myapikey = "";
+        private readonly string url = "https://reqres.in/api/users";
+
+        public async Task<List<User>> GetUsers()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", myapikey);
+
+            var result = await client.GetAsync(url);// /users
+            var jsonString = await result.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+            return data != null ? data.data.ToList() : [];
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", myapikey);
+
+            var result = await client.GetAsync(url+"/"+id);// /users/1
+            var jsonString = await result.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<RootobjectSingle>(jsonString);
+            return data != null ? data.data : null;
+        }
+    }
+}
